8a8faa9d22fc65044aa7c915cd8a5634d93492d7
Farouk Abdi <abdifarouk58@gmail.com>
2025-04-02 16:55:39 +0300

    added to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  // e.preventDefault()
+  let formInput = document.querySelector('#create-task-form')
+  // console.log(formInput);
+  let formText = formInput[0]
+  // console.log(formText);
+  formInput.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    todo(formText.value)
+    formInput.reset()
+  })
 });
+
+function todo(formText){
+  let myTodos =document.querySelector('#list #tasks')
+  console.log(myTodos);
+  let liItem = document.createElement('li');
+  liItem.textContent = formText;
+  myTodos.prepend(liItem);
+
+}
+todo();
