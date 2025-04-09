a7c249f42e6b769209f2b56022ce79d5638feefd
Farouk Abdi <abdifarouk58@gmail.com>
2025-04-09 21:56:32 +0300

    added to tasklister

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  // e.preventDefault()
+document.addEventListener("DOMContentLoaded", ()=>{
+  handlingFormSubmission();
+})
+
+function handlingFormSubmission(){
+
   let form = document.querySelector('#create-task-form')
-  // console.log(formInput);
-  let formText = form[0]
-  // console.log(formText);
-  form.addEventListener('submit', (e)=>{
+  console.log(form)
+  let  formInput = document.querySelector('#new-task-description')
+  console.log(formInput)
+  let  taskList = document.querySelector('#tasks')
+  // taskList.style.listStyle ="none"
+  console.log(taskList)
+  let submitBtn = form[1];
+
+
+
+
+form.addEventListener("submit",(e)=>{
     e.preventDefault()
-    todo(formText.value)
-    form.reset()
-  })
-});
+    let li = document.createElement("li")
+    li.textContent = formInput.value
+    taskList.append(li)
+    formInput.value = ""
+  // console.log(e)
 
-function todo(formText){
-  let taskList =document.querySelector('#list #tasks')
-  console.log(taskList);
-  let liItem = document.createElement('li');
-  liItem.textContent = 'Wash the dishes';
-  taskList.prepend(liItem);
-  let button = document.createElement('button');
-  button.textContent='delete'
-  taskList.append(button);
-  button.addEventListener('click',()=>{
-    liItem.remove();
-  })
-
-}
-todo();
+  });
+};
